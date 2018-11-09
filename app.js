f03f8c915c6b6854aeb14ff6102c9b6bd6d22ba6
Koxinga <timwu1015@gmail.com>
2018-11-08 22:38:55 -0500

    functional game

---
+++ app.js
@@ -0,0 +1,123 @@
+var config = {
+    apiKey: "AIzaSyCMtZBkG6LjJMer0GCMM9YIqXvyOdw-dEk",
+    authDomain: "rps-game-5de7b.firebaseapp.com",
+    databaseURL: "https://rps-game-5de7b.firebaseio.com",
+    projectId: "rps-game-5de7b",
+    storageBucket: "rps-game-5de7b.appspot.com",
+    messagingSenderId: "83002717061"
+  };
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+var player = database.ref("/players");
+var guess = database.ref("/guesses");
+
+var name1 = "";
+var name2 = "";
+
+var pick1 = "";
+var pick2 = "";
+
+function checkthewinner(guess1, guess2) {
+    if ((guess1 === "r") && (guess2 === "s")) {
+        return 1;
+    } else if ((guess1 === "r") && (guess2 === "p")) {
+        return 2;
+    } else if ((guess1 === "s") && (guess2 === "r")) {
+        return 2;
+    } else if ((guess1 === "s") && (guess2 === "p")) {
+        return 1;
+    } else if ((guess1 === "p") && (guess2 === "r")) {
+        return 1;
+    } else if ((guess1 === "p") && (guess2 === "s")) {
+        return 2;
+    } else if (guess1 === guess2) {
+        return 3;
+    }
+}
+
+function reset() {
+    $("#text1").empty();
+    $("#text2").empty();
+    $("#result").empty();
+
+    guess.set({
+        guess1: "",
+        guess2: ""
+    })
+
+    pick1 = "";
+    pick2 = "";
+}
+
+$("#submit1").on("click", function(event) {
+    event.preventDefault();
+    
+    name1 = $("#name1").val().trim();
+    $("#player1").text(name1);
+    $("#name1").val("");
+
+
+    player.set({
+        player1: name1,
+        player2: name2
+    });
+});
+
+$("#submit2").on("click", function(event) {
+    event.preventDefault();
+    
+    name2 = $("#name2").val().trim();
+    $("#player2").text(name2);
+    $("#name2").val("");
+
+    player.set({
+       player1: name1,
+       player2: name2 
+    });
+});
+
+player.on("value", function(snap) {
+    $("#player1").text(snap.val().player1);
+    $("#player2").text(snap.val().player2);
+
+    name1 = snap.val().player1;
+    name2 = snap.val().player2;
+});
+
+$(".button1").on("click", function() {
+    pick1 = $(this).val();
+    $("#text1").text("wait for your opponent");
+
+    guess.set({
+        guess1: pick1,
+        guess2: pick2
+    });
+});
+
+$(".button2").on("click", function() {
+    pick2 = $(this).val();
+    $("#text2").text("wait for your opponent");
+
+    guess.set({
+        guess1: pick1,
+        guess2: pick2
+    });
+});
+
+guess.on("value", function(snap) {
+    if ((snap.val().guess1) && (snap.val().guess2)) {
+        var result = checkthewinner(pick1, pick2);
+        if (result === 1) {
+            $("#result").text(name1 + " won!!!");
+            setTimeout(reset, 3000);
+        } else if (result === 2) {
+            $("#result").text(name2 + " won!!!");
+            setTimeout(reset, 3000);
+        } else {
+            $("#result").text("It is a tie!!!");
+            setTimeout(reset, 3000);
+        }
+    }
+});
